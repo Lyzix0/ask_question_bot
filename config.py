0ff9a4042439318abca7bca53ae89573013f433f
Maxim Valiev <123410376+Lyzix0@users.noreply.github.com>
2024-07-31 16:58:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import logging
 
-BOT_TOKEN = "bot_token"
+BOT_TOKEN = "bot_token" # api ключ бота
 
-GUILD_ID = 11212321321
-PRIVATE_CHANNEL_ID = 12321
-PUBLIC_CHANNEL_ID = 12312321321
+GUILD_ID = 11212321321 # айди сервера
+PRIVATE_CHANNEL_ID = 12321 # айди приватного канала с вопросами
+PUBLIC_CHANNEL_ID = 12312321321 # айди публичного канала с ответами
 
 logging.basicConfig(filename="logs/logs.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
